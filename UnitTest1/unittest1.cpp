10bdb27fd61b5f2ecda24672af42edcda1b76496
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-04-05 14:50:36 +0900

    簡単なアセンブラを実行するテストケースを作成した

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -2,6 +2,8 @@
 #include "CppUnitTest.h"
 #include "../x86Emulator/Emulator.hpp"
 #include <iostream>
+#include <vector>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
@@ -16,9 +18,15 @@ namespace UnitTest1
 			static constexpr size_t MEMORY_SIZE = (1024 * 1024);
 				//EmulatorÇê∂ê¨
 				x86::Emulator emu(MEMORY_SIZE, 0x0000, 0x7c00);
-				std::istringstream ist("abc");
-				emu.Read(std::move(ist));
-			
+				std::vector<uint8_t> code = { 0xb8,0x29,0x00,0x00,0x00,0xeb,0xf9 };
+				emu.Read(code.cbegin(),code.cend());
+				auto success = []() {
+					Logger::WriteMessage("end of program");
+				};
+				auto fail = []() {
+					Assert::Fail(L"Not Implemented");
+				};
+				emu.exeute(success,fail);
 		}
 	};
 }
