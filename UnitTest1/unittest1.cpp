ee4085d69c2de66f62c199caf456658e8dc30266
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-04-25 11:05:46 +0900

    modrmのテストを追加

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -60,5 +60,26 @@ namespace UnitTest1
 			emu.Exeute(success, fail, trace);
 			Logger::WriteMessage(emu.Dump_registers().c_str());
 		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			/* ƒƒ‚ƒŠ‚Í1MB */
+			static constexpr size_t MEMORY_SIZE = (1024 * 1024);
+			//Emulator‚ğ¶¬
+			x86::Emulator emu(MEMORY_SIZE, 0x7c00, 0x7c00);
+			auto ifs = file_load_helper("../testAsm/modrm-test.bin");
+			emu.Read(ifs);
+			auto success = []() {
+				Logger::WriteMessage("end of program");
+			};
+			auto fail = []() {
+				Assert::Fail(L"Not Implemented");
+			};
+			auto trace = [](std::wstring str) {
+				Logger::WriteMessage(str.c_str());
+			};
+			emu.Exeute(success, fail, trace);
+			Logger::WriteMessage(emu.Dump_registers().c_str());
+		}
 	};
 }
