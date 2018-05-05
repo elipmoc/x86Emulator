c1d95b79cca055e05246bb7c19846176f71a2968
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-05-05 11:51:07 +0900

    Eflagsの実装

---
+++ x86Emulator/Eflags.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <cinttypes>
+namespace x86 {
+	class Eflags {
+		//EFLAGSÉåÉWÉXÉ^
+		uint32_t eflags;
+		static constexpr int carry_flag = 1;
+		static constexpr int zero_flag = 1 << 6;
+		static constexpr int sign_flag = 1 << 7;
+		static constexpr int overflow_flag = 1 << 11;
+
+		template<int F>
+		void set(bool flag) {
+			eflags = flag?eflags | F : eflags & ~F;
+		}
+
+	public:
+		void update_eflags_sub(uint32_t v1, uint32_t v2, uint64_t result) {
+			int sign1 = v1 >> 31;
+			int sign2 = v2 >> 31;
+			int signr = (result >> 31) & 1;
+			set<carry_flag>(result >> 32);
+			set<zero_flag>(result==0);
+			set<sign_flag>(signr);
+			set<overflow_flag>(sign1 != sign2 && sign1 != signr);
+		}
+	};
+}
