4db11f40a3971c0a4b79c0f783ce8f7c04010e58
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-05-08 14:41:48 +0900

    Eflagsの各種フラグの状態を取得する関数を実装した

--- x86Emulator/Eflags.hpp
+++ x86Emulator/Eflags.hpp
@@ -2,19 +2,30 @@
 #include <cinttypes>
 namespace x86 {
 	class Eflags {
+
 		//EFLAGSÉåÉWÉXÉ^
 		uint32_t eflags;
+
+	public:
+
 		static constexpr int carry_flag = 1;
 		static constexpr int zero_flag = 1 << 6;
 		static constexpr int sign_flag = 1 << 7;
 		static constexpr int overflow_flag = 1 << 11;
 
+	private:
 		template<int F>
 		void set(bool flag) {
 			eflags = flag?eflags | F : eflags & ~F;
 		}
 
 	public:
+
+		template<int F>
+		bool get() const{
+			return eflags & F;
+		}
+
 		void update_eflags_sub(uint32_t v1, uint32_t v2, uint64_t result) {
 			int sign1 = v1 >> 31;
 			int sign2 = v2 >> 31;
